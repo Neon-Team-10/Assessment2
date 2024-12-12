8b15768fd72640c7726ea0a4c9ffbd0fd5bd7b8e
Ayman-Hachkal <ayman.hachkal@gmail.com>
2024-12-12 16:45:38 +0000

    added pause class

--- src/main/java/io/github/neonteam10/ui/PauseButton.java
+++ src/main/java/io/github/neonteam10/ui/PauseButton.java
@@ -2,11 +2,16 @@ package io.github.neonteam10.ui;
 
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import io.github.neonteam10.GameLogic;
 
 public class PauseButton extends Table {
     ImageButton pause;
+    private final UiAssets uiAssets;
+    private final GameLogic gameLogic;
 
-    public PauseButton() {
+    public PauseButton(UiAssets uiAssets, GameLogic gameLogic) {
+        this.uiAssets = uiAssets;
+        this.gameLogic = gameLogic;
     }
 
 }
