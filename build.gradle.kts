acebd83c4ba8bc46b915b87228211c80f747ee18
JamesF1969 <zmk508@york.ac.uk>
2025-01-08 20:54:40 +0000

    JaCoCO

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java-library")
+    id("jacoco")
     id("com.gradleup.shadow") version "8.3.3"
 }
 
-group = "io.github.uoyteamsix"
-version = "1.0"
+group = "io.github.neonteam10"
+version = "2.0"
 
 repositories {
     mavenCentral()
@@ -24,7 +25,10 @@ dependencies {
     
         //testRuntimeOnly("org.junit.platform:junit-platform-engine")
 }
-
+jacoco {
+    toolVersion = "0.8.12"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
 tasks.compileJava {
     options.release = 17;
 }
@@ -40,11 +44,16 @@ tasks.jar {
 tasks.shadowJar {
     minimize()
 }
-/*
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
-*/
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
 }
+
